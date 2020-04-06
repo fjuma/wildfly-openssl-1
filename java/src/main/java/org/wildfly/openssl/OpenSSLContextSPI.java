25c227c35b30bd8b2854fa825ac7ec39a356582a
Farah Juma <fjuma@redhat.com>
2020-04-06 15:24:24 -0400

    only set no op ticket if not using tls 1.3

--- java/src/main/java/org/wildfly/openssl/OpenSSLContextSPI.java
+++ java/src/main/java/org/wildfly/openssl/OpenSSLContextSPI.java
@@ -174,7 +174,7 @@ public abstract class OpenSSLContextSPI extends SSLContextSpi {
             boolean disableSessionTicketsSupported = false;
             try {
                 disableSessionTicketsSupported = SSL.getInstance().hasOp(SSL.SSL_OP_NO_TICKET);
-                if (disableSessionTicketsSupported) {
+                if (disableSessionTicketsSupported && !isTLS13Supported()) {
                     SSL.getInstance().setSSLContextOptions(ctx, SSL.SSL_OP_NO_TICKET);
                 }
             } catch (UnsatisfiedLinkError e) {
